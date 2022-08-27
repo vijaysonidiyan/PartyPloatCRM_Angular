9c7042b0e1300ae7d0700c3aeebf2ab7aea1fa3b
Bhargav Sankaliya <bhargav_dtl@outlook.com>
2022-08-27 10:40:28 +0530

    add inquiry changes done

--- src/app/back-office/add-inquiry/add-inquiry.component.ts
+++ src/app/back-office/add-inquiry/add-inquiry.component.ts
@@ -4,8 +4,8 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { AdminLayoutService } from "app/layouts/admin-layout/admin-layout.service";
 import { CommonService } from "app/shared/common.service";
 import * as moment from 'moment';
-import {MomentDateAdapter} from '@angular/material-moment-adapter';
-import {DateAdapter, MAT_DATE_FORMATS,ThemePalette, MAT_DATE_LOCALE} from '@angular/material/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, ThemePalette, MAT_DATE_LOCALE } from '@angular/material/core';
 
 export const MY_FORMATS = {
   parse: {
@@ -24,8 +24,8 @@ export const MY_FORMATS = {
   templateUrl: './add-inquiry.component.html',
   styleUrls: ['./add-inquiry.component.css'],
   providers: [
-    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
-    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
   ],
 })
 export class AddInquiryComponent implements OnInit {
@@ -44,7 +44,7 @@ export class AddInquiryComponent implements OnInit {
   public enableMeridian = false;
   public minDate = new Date();
   public minEndDate = {};
-  public maxDate: moment.Moment;
+  public maxDate = new Date();
 
   public stepHour = 1;
   public stepMinute = 1;
@@ -53,6 +53,7 @@ export class AddInquiryComponent implements OnInit {
   // startDateObj = '';
   // endDateObj = '';
   selectedDate: any
+  minEndTime = {};
   //calender done
 
   get fclientinquiryData() {
@@ -64,9 +65,12 @@ export class AddInquiryComponent implements OnInit {
 
     this.route.queryParams.subscribe((queryParams) => {
       debugger
-      if (!!queryParams.startDate ) {
-      // if (!!queryParams.startDate && !!queryParams.endDate) {
+      if (!!queryParams.startDate) {
+        // if (!!queryParams.startDate && !!queryParams.endDate) {
         this.selectedDate = queryParams.startDate;
+        this.minDate = new Date(queryParams.startDate);
+        let maxDate = new Date(this.selectedDate).getFullYear() + '-' + (new Date(this.selectedDate).getMonth() + 1) + '-' + new Date(this.selectedDate).getDate();
+        this.maxDate = new Date(maxDate + ' ' + '23:59');
         // this.startDateObj = queryParams.startDate;
         // this.endDateObj = queryParams.endDate;
       }
@@ -88,7 +92,14 @@ export class AddInquiryComponent implements OnInit {
 
   onStartDateChange(data: any, index: any) {
     this.minEndDate[index] = data._d;
-    (((this.clientinquiryDataForm.controls['events'] as FormArray).controls[index] as FormGroup).controls['endDateObj'].setValue(''));
+    (((this.clientinquiryDataForm.controls['events'] as FormArray).controls[index] as FormGroup).controls['endDateObj'].setValue(data._d));
+  }
+
+  onStartTimeChange(time: any, index: any) {
+    debugger
+    let maxDate = new Date(this.selectedDate).getFullYear() + '-' + (new Date(this.selectedDate).getMonth() + 1) + '-' + new Date(this.selectedDate).getDate();
+    this.minEndTime[index] = new Date(maxDate + ' ' + time.target.value);
+    // this.minEndTime[index] = time.target.value
   }
 
   getEventActiveList() {
@@ -121,10 +132,10 @@ export class AddInquiryComponent implements OnInit {
     return this.fb.group({
       eventType: [(oItem['eventType'] ? oItem['eventType'] : null)],
       guest: [(oItem['guest'] ? oItem['guest'] : '')],
-      // startDateObj: [(oItem['startDateObj'] ? oItem['startDateObj'] : !!this.startDateObj ? new Date(this.startDateObj) : '')],
-      // endDateObj: [(oItem['endDateObj'] ? oItem['endDateObj'] : !!this.endDateObj ? new Date(this.endDateObj) : '')],
-      startTimeObj: [(oItem['endTimeObj'] ? oItem['endTimeObj'] : '')],
-      endTimeObj: [(oItem['endTimeObj'] ? oItem['endTimeObj'] : '')],
+      startDateObj: [(oItem['startDateObj'] ? oItem['startDateObj'] : !!this.selectedDate ? new Date(this.selectedDate) : '')],
+      endDateObj: [(oItem['endDateObj'] ? oItem['endDateObj'] : !!this.selectedDate ? new Date(this.selectedDate) : '')],
+      // startTimeObj: [(oItem['endTimeObj'] ? oItem['endTimeObj'] : '')],
+      // endTimeObj: [(oItem['endTimeObj'] ? oItem['endTimeObj'] : '')],
     });
   }
 
@@ -143,57 +154,57 @@ export class AddInquiryComponent implements OnInit {
       this.submittedclientInquiryData = true;
       return;
     }
-    let eventObjList = [];
-    this.clientinquiryDataForm.controls.events.value.map((x: any) => {
-      // if (!!x.endDateObj._d && !!x.startDateObj._d) {
-      //   let eventObj = {
-      //     eventType: x.eventType,
-      //     guest: x.guest,
-      //     startDateObj: x.startDateObj._d,
-      //     endDateObj: x.endDateObj._d
-      //   }
-      //   eventObjList.push(eventObj);
-      // }
-      // else if (!!x.startDateObj._d && x.endDateObj) {
-      //   let eventObj = {
-      //     eventType: x.eventType,
-      //     guest: x.guest,
-      //     startDateObj: x.startDateObj._d,
-      //     endDateObj: x.endDateObj
-      //   }
-      //   eventObjList.push(eventObj);
-      // }
-      // else if (!!x.endDateObj._d && x.startDateObj) {
-      //   let eventObj = {
-      //     eventType: x.eventType,
-      //     guest: x.guest,
-      //     startDateObj: x.startDateObj,
-      //     endDateObj: x.endDateObj._d
-      //   }
-      //   eventObjList.push(eventObj);
-      // }
-      // else if (x.endDateObj && x.startDateObj) {
-      //   let eventObj = {
-      //     eventType: x.eventType,
-      //     guest: x.guest,
-      //     startDateObj: x.startDateObj,
-      //     endDateObj: x.endDateObj
-      //   }
-      //   eventObjList.push(eventObj);
-      // }
-      let selectedDate = new Date(this.selectedDate).getFullYear() + '-' + (new Date(this.selectedDate).getMonth() + 1) + '-' + new Date(this.selectedDate).getDate();
+    // let eventObjList = [];
+    // this.clientinquiryDataForm.controls.events.value.map((x: any) => {
+    //   // if (!!x.endDateObj._d && !!x.startDateObj._d) {
+    //   //   let eventObj = {
+    //   //     eventType: x.eventType,
+    //   //     guest: x.guest,
+    //   //     startDateObj: x.startDateObj._d,
+    //   //     endDateObj: x.endDateObj._d
+    //   //   }
+    //   //   eventObjList.push(eventObj);
+    //   // }
+    //   // else if (!!x.startDateObj._d && x.endDateObj) {
+    //   //   let eventObj = {
+    //   //     eventType: x.eventType,
+    //   //     guest: x.guest,
+    //   //     startDateObj: x.startDateObj._d,
+    //   //     endDateObj: x.endDateObj
+    //   //   }
+    //   //   eventObjList.push(eventObj);
+    //   // }
+    //   // else if (!!x.endDateObj._d && x.startDateObj) {
+    //   //   let eventObj = {
+    //   //     eventType: x.eventType,
+    //   //     guest: x.guest,
+    //   //     startDateObj: x.startDateObj,
+    //   //     endDateObj: x.endDateObj._d
+    //   //   }
+    //   //   eventObjList.push(eventObj);
+    //   // }
+    //   // else if (x.endDateObj && x.startDateObj) {
+    //   //   let eventObj = {
+    //   //     eventType: x.eventType,
+    //   //     guest: x.guest,
+    //   //     startDateObj: x.startDateObj,
+    //   //     endDateObj: x.endDateObj
+    //   //   }
+    //   //   eventObjList.push(eventObj);
+    //   // }
+    //   let selectedDate = new Date(this.selectedDate).getFullYear() + '-' + (new Date(this.selectedDate).getMonth() + 1) + '-' + new Date(this.selectedDate).getDate();
 
-      let startDateObj = new Date(selectedDate + ' ' + x.startTimeObj);
-      let endDateObj = new Date(selectedDate + ' ' + x.endTimeObj);
+    //   let startDateObj = new Date(selectedDate + ' ' + x.startTimeObj);
+    //   let endDateObj = new Date(selectedDate + ' ' + x.endTimeObj);
 
-      let eventObj = {
-        eventType: x.eventType,
-        guest: x.guest,
-        startDateObj: startDateObj,
-        endDateObj: endDateObj
-      }
-      eventObjList.push(eventObj);
-    })
+    //   let eventObj = {
+    //     eventType: x.eventType,
+    //     guest: x.guest,
+    //     startDateObj: startDateObj,
+    //     endDateObj: endDateObj
+    //   }
+    //   eventObjList.push(eventObj);
+    // })
 
     let clientinquiryModelObj = {
       name: this.clientinquiryDataForm.controls.name.value,
@@ -201,7 +212,7 @@ export class AddInquiryComponent implements OnInit {
       primaryContact: this.clientinquiryDataForm.controls.primaryContact.value,
       secondryContact: this.clientinquiryDataForm.controls.secondryContact.value,
       address: this.clientinquiryDataForm.controls.address.value,
-      events: eventObjList
+      events: this.clientinquiryDataForm.controls.events.value
     };
     // return
     this.adminLayoutService.createClientinquiry(clientinquiryModelObj).subscribe(
